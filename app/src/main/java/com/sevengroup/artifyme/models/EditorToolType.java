a3c17616195be5b50804c04edb34e1b2ca7e39ec
Kien Ha <23120056@student.hcmus.edu.vn>
2025-12-19 08:18:49 +0700

    add generate image

--- app/src/main/java/com/sevengroup/artifyme/models/EditorToolType.java
+++ app/src/main/java/com/sevengroup/artifyme/models/EditorToolType.java
@@ -10,7 +10,6 @@ public enum EditorToolType {
     BG_REMOVE_LOCAL(R.string.tool_remove_bg_local, R.drawable.remove_bg_24px),
     BG_REMOVE_API(R.string.tool_remove_bg_api, R.drawable.remove_bg_24px);
 
-
     private final int nameResId;
     private final int iconResId;
 
